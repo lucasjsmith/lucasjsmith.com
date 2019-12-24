2d0f77c4545ce194411cfd422be0b2730556d772
lucasjsmith <lucasjsmith@users.noreply.github.com>
2019-12-24 11:48:29 -0800

    resync with web contents

--- site/snippets/social_cards.php
+++ site/snippets/social_cards.php
@@ -4,12 +4,12 @@
   <meta name="twitter:creator" content="@LucasJSmith">
   <meta name="twitter:title" content= <?= "\"" . $page->title()->html() . " | " . $site->title() . "\"" ?> >
   <meta name="twitter:description" content= <?= "\"" . $page->text()->kirbytext()->excerpt(50, 'words') . "\"" ?> >
-  <meta name="twitter:image" content= <?= "\"" .  $page->coverimage()->toUrl() . "\"" ?> >
+  <meta name="twitter:image" content= <?= "\"" .  $page->url() ."/" . $page->coverimage()->filename() . "\"" ?> >
 
 
   <!-- OpenGraph / Facebook card -->
   <meta property="og:title" content= <?= "\"" . $page->title()->html() . " | " . $site->title() . "\"" ?> >
   <meta property="og:type" content="website" />
   <meta property="og:url" content= <?= "\"" . $page->url() . "\"" ?> >
-  <meta property="og:image" content= <?= "\"" .  $page->coverimage()->toUrl() . "\"" ?> >
+  <meta property="og:image" content= <?= "\"" .  $page->url() ."/" . $page->coverimage()->filename() . "\"" ?> >
   <meta property="og:description" content= <?= "\"" . $page->text()->kirbytext()->excerpt(50, 'words') . "\"" ?> >
